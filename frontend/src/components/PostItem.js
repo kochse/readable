934d5af9c95542112039e58ee7a562a9f47af207
Sebastian Koch <kochsebastian@gmail.com>
2017-12-18 16:54:45 +0100

    fix: post url

--- frontend/src/components/PostItem.js
+++ frontend/src/components/PostItem.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const PostItem = ({ post, upVotePost, downVotePost }) => (
   <li className="list-group-item" key={post.id}>
     <div className="mt-2 mb-2 d-flex justify-content-between"><span>Score: {post.voteScore}</span><span>{new Date(post.timestamp).toDateString()}</span><div><a href="">edit</a> <a href="">delete</a></div></div>
-    <div className="mt-2 mb-2"><Link to={`/view/${post.id}`}><h3>{post.title}</h3></Link></div>
+    <div className="mt-2 mb-2"><Link to={`/${post.category}/${post.id}`}><h3>{post.title}</h3></Link></div>
     <div className="mb-2"><span>Author: {post.author}</span> </div>
     <div className="mb-2"><span>{post.commentCount} Comments</span></div>
     <div className="mb-2 btn-group" role="group">
